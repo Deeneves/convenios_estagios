7c8a980428e0cd5b5d68e424d9116e2d28e7c77a
André Neves <106282513+Deeneves@users.noreply.github.com>
2026-01-29 16:34:56 -0300

    Remove contrapartida app and update project settings and URLs to include academico and usuarios apps.

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.contrapartida',
+    'apps.academico',
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
