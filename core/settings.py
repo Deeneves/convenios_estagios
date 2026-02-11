76644d958eeba597e89e6ffa02660d7a8956e71c
André Neves <106282513+Deeneves@users.noreply.github.com>
2026-02-10 21:30:54 -0300

    Update requirements and configure static file storage: Add new dependencies including charset-normalizer, django-environ, gunicorn, and others to requirements.txt. Integrate Whitenoise middleware for static file handling in settings.py and define storage backends for static files.

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,6 +96,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Autenticação
 LOGIN_REDIRECT_URL = 'home'
